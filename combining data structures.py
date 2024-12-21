47fcb98765c0a51145a212e4c3342fa828b29d5e
Toufiq-Javid <talibjavid2018@gmail.com>
2024-12-21 21:39:34 +0530

    Add files via upload

---
+++ combining data structures.py
@@ -0,0 +1,13 @@
+# #Create a dictionary where each key is a student's name, and the value is a tuple containing
+# their scores in three subjects.
+#  Write functions to:
+# 1. Calculate and print the average score of each student.
+# 2. Find the student with the highest average score.
+
+
+# marks_stud={
+#     "Faheem": (77,66,88),
+#     "Zamin": (87,98,78),
+#     "Aqleem": (96,86,93)
+# }
+
